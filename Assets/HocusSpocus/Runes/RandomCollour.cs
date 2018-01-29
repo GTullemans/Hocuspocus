cace555d0c175312b21c5a8171cf44fcf55af717
Geert Tullmans <519753@student.glu.nl>
2018-01-29 12:38:58 +0100

    BeginConcept HocusSpocus

---
+++ Assets/HocusSpocus/Runes/RandomCollour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCollour : MonoBehaviour {
+    [SerializeField]
+    private GameObject _Runes;
+    private GameObject[] _RuneArray;
+    [SerializeField]
+    private List<Color> _Color;
+    [SerializeField]
+    private float _TimeNominal;
+    private float _Timer;
+	// Use this for initialization
+	void Start () {
+        _RuneArray = new GameObject[_Runes.transform.childCount];
+		for(int i = 0; i < _Runes.transform.childCount; i++)
+        {
+            _RuneArray[i] = _Runes.transform.GetChild(i).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _Timer += Time.deltaTime;
+        if(_Timer > Random.Range(_TimeNominal - 5, _TimeNominal + 5))
+        {
+            GameObject ChosenOne = _RuneArray[Random.Range(0, _RuneArray.Length - 1)];
+            ChosenOne.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = _Color[Random.Range(0, _Color.Count)];
+            _Timer = 0;
+        }
+	}
+}
---
+++ Assets/HocusSpocus/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    [SerializeField]
+    private Vector3 _Rotatie;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        gameObject.transform.Rotate(_Rotatie * Time.fixedDeltaTime);
+
+        
+    }
+}
